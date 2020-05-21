86d2420bea3b05c43c414240ad61812d79d9e615
santiago <nevadosantiago97@gmail.com>
2020-05-20 23:15:23 -0300

    Validaciones Admin,Centro,Consul

--- Grupo1.AgendaDeTurnos/Models/Consultorio.cs
+++ Grupo1.AgendaDeTurnos/Models/Consultorio.cs
@@ -9,6 +9,12 @@ namespace Grupo1.AgendaDeTurnos.Models
     public class Consultorio
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [MaxLength(100, ErrorMessage = "La longitud máxima del campo es de 100 caracteres")]
+        [MinLength(2, ErrorMessage = "La longitud mínima del campo es de 2 caracteres")]
+        [RegularExpression("[A-Za-z áéíóú]*", ErrorMessage = "El campo solo admite letras")]
+        [Display(Name = "Nombre")]
         public string Nombre { get; set; }
         public List<Disponibilidad> Disponibilidades { get; set; }
         public List<Turno> Turnos { get; set; }
